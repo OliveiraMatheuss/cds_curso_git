ad1e7533ea68fb92dbbb5e3fd039278147e982c8
Matheus Oliveira <86089038+OliveiraMatheuss@users.noreply.github.com>
2023-04-24 11:01:17 -0300

    Adicionado func create_answers_section no def main

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from scr import load_data
 from scr import create_dataframe_section
+from scr import create_answers_section
 def main():
     df_raw = load_data()
     create_dataframe_section(df_raw)
+    create_answers_section(df_raw)
     st.dataframe(df_raw)
     
 if __name__ == '__main__':
