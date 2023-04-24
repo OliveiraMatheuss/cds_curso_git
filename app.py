9596af6ab821f0e4b69ac8f3ba9f565ba6218d97
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-04-24 10:50:39 -0300

    Adicionado a função create_dataframe_section ao df main do app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from scr import load_data
-
+from scr import create_dataframe_section
 def main():
     df_raw = load_data()
-    
+    create_dataframe_section(df_raw)
     st.dataframe(df_raw)
     
 if __name__ == '__main__':
     main()
+    
+    
